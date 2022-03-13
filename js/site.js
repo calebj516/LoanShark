6d4fa762eba95118447e3d42b343167a324e50d7
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-03-12 21:44:42 -0600

    Loan Shark: JS
    Completed getValues() function which retrieves the loan amount, term, and interest rate values from the page, and started on the function that calculates the results.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,31 @@
-// call Hello World
-function HelloWorld() {
-  alert("Hello World!");
+// get values
+function getValues() {
+
+  let loanAmt = +(document.getElementById('loanAmt').value);
+  let term = +(document.getElementById('term').value);
+  let rate = +(document.getElementById('interestRate').value);
+
+  const results = calcResults(loanAmt, term, rate);
 }
+
+// calculate results
+function calcResults(loan, term, intRate) {
+
+  let monthlyPayment = loan * (intRate / 1200) / (1 - (1 + intRate / 1200) ** (-term));
+  console.log(monthlyPayment);
+  let totalCost = monthlyPayment * term;
+  console.log(totalCost);
+  let totalInterest = totalCost - loan;
+  console.log(totalInterest); 
+
+  let remainingBal = loan;
+  let intPayment = remainingBal * (intRate / 1200);
+  let principal = monthlyPayment - intPayment;
+
+  // while(remainingBal >= 0) {
+
+  // }
+
+}
+
+// display results
